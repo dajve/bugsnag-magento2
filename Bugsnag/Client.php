75063eb1eabd8404d0c6c4b4a25acb533446544b
Chris Florin <chrisflorin@users.noreply.github.com>
2018-03-15 10:30:13 -0600

    Added Request Information

--- Bugsnag/Client.php
+++ Bugsnag/Client.php
@@ -8,7 +8,9 @@
 namespace Interjar\BugSnag\Bugsnag;
 
 use Bugsnag\Client as BugSnag_Client;
+use Bugsnag\Report;
 use Interjar\BugSnag\Helper\Config;
+use Magento\Framework\App\Request\Http;
 
 class Client extends BugSnag_Client
 {
@@ -22,17 +24,42 @@ class Client extends BugSnag_Client
      * Get API KEY from env.php and create instance of BugSnag_Client
      *
      * @param Config $config
+     * @param Http $request
      */
     public function __construct(
-        Config $config
+        Config $config,
+        Http $request
     )
     {
         $this->config = $config->getConfiguration();
         if($this->config) {
             parent::__construct($this->config, null, parent::makeGuzzle());
-            
+
             \Bugsnag\Handler::register($this);
+
+            $this->registerCallback(function (Report $report) use ($request) {
+                $report->addMetaData([
+                    'request' => [
+                        'base_path' => $request->getBasePath(),
+                        'base_url' => $request->getBaseUrl(),
+                        'client_ip' => $request->getClientIp(),
+                        'controller_name' => $request->getControllerName(),
+                        'distro_base_url' => $request->getDistroBaseUrl(),
+                        'front_name' => $request->getFrontName(),
+                        'full_action_name' => $request->getFullActionName(),
+                        'host' => $request->getHttpHost(),
+                        'is_ajax' => $request->isAjax(),
+                        'params' => $request->getParams(),
+                        'path_info' => $request->getPathInfo(),
+                        'method' => $request->getMethod(),
+                        'protocol' => $request->getScheme(),
+                        'request_string' => $request->getRequestString(),
+                        'route' => $request->getRouteName(),
+                        'uri' => $request->getRequestUri(),
+                        'user_params' => $request->getUserParams()
+                    ]
+                ]);
+            });
         }
     }
-
 }
